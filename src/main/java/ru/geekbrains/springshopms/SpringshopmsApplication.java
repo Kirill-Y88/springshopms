0cac02de7b3599f76b20bf3688b481a5c1722df5
Kirill <benberov09@mail.ru>
2021-04-15 12:14:42 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/springshopms/SpringshopmsApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.springshopms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringshopmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringshopmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/geekbrains/springshopms/SpringshopmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.springshopms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringshopmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
